3e105c5bd987e537be991f312855441c3cfcc7f5
CptGeo <georgekalyvianakis18@gmail.com>
2021-03-20 23:52:52 +0200

    Created GoodnetView.js as functional for testing

---
+++ components/GoodnetViewF.js
@@ -0,0 +1,52 @@
+import React, {Component, useContext, useState} from "react";
+import {WebView} from "react-native-webview";
+import {Text, StyleSheet, View, ActivityIndicator} from "react-native";
+import { SettingsContext } from "../App";
+import {goodnetSettings as gn} from "../settings/goodnetSettings";
+
+
+
+export default function GoodnetViewF(props){
+    const settings = useContext(SettingsContext);
+    return(
+        <WebView 
+            source={
+                {
+                    uri : gn.url,
+                    headers : {
+                        Cookie: 'goodnet_mobile=1'
+                    }
+                }
+            }
+            sharedCookiesEnabled={true}
+            styles={styles.container}
+            startInLoadingState={true}
+            pullToRefreshEnabled={true}
+            scalesPageToFit={true}
+            renderLoading={() => (
+                <ActivityIndicator 
+                    color='#4374ca' 
+                    size='large' 
+                    style={styles.container}
+                />)}
+            // onNavigationStateChange={this.webviewNavigationChange}
+            // onShouldStartLoadWithRequest={this.shouldStartLoadWithRequest}
+        />
+    );
+
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        flexGrow: 1,
+        alignItems : 'center',
+        justifyContent : "center",
+        position : "absolute",
+        width : "100%",
+        height : "100%",
+        left: 0,
+        top : 0
+    },
+});
