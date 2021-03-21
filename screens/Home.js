198bf6d52673d9b5999e8d2f7d1b5f7659b930fb
CptGeo <georgekalyvianakis18@gmail.com>
2021-03-21 21:22:36 +0200

    Cleaned it a bit. Changed some styling.

--- screens/Home.js
+++ screens/Home.js
@@ -5,30 +5,19 @@ import GoodnetViewF from "../components/GoodnetViewF";
 import CategoriesMenuFetcher from "../components/CategoriesMenuFetcher";
 import React from "react";
 import {View, Text, StyleSheet} from "react-native";
-import Navi from "../components/Navi";
 
-const Home = (props) => {
+export default Home = (props) => {
 
     return (
-        <View style={styles.container}>
-            <GoodnetViewF style={styles.container}/>
-            <CategoriesMenuFetcher/>
-            {/* <Navi /> */}
-        </View>
-        // <Text>Hello!</Text>
+      <GoodnetViewF style={styles.container}/>
     )
-
 }
 
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      height: 300,
-      width: "100%",
-      position : "relative",
-      zIndex : 10
+      justifyContent : "center",
+      alignItems : "center"
     }
   });
-
-export default Home;
