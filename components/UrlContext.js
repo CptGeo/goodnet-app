5c49bd7e628fdd412c22d4c047a6117d935342c4
CptGeo <georgekalyvianakis18@gmail.com>
2021-03-26 00:42:35 +0200

    Cleaned the context component to use

--- components/UrlContext.js
+++ components/UrlContext.js
@@ -1,22 +1,5 @@
-import React, {createContext, useContext, useState} from "react";
-import {View, Text, StyleSheet} from "react-native";
-
-
+import {createContext} from "react";
 
 
 export default UrlContext = createContext();
 
-
-const Provider = props =>{
-
-    const [url, setUrl] = useState("https://goodnet.gr");
-
-    const value = {state : {url}, actions : {setUrl}};
-
-    return (
-        <UrlContext.Provider value={value}>
-            {props.children}
-        </UrlContext.Provider>
-    );
-    
-}
