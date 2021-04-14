d081dabbbf961a3db03e813fb4c9421e91c6dbc4
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-15 00:46:40 +0300

    Added more streams

--- settings/streams.js
+++ settings/streams.js
@@ -1,18 +1,43 @@
 export default playlists = [
-    {
+    {   key : 1,
         title : "Team FM",
         url : "http://185.78.220.57:9200/stream.mp3",
         icon : "https://cdn.e-radio.gr/logos/gr/big/teamfm.png",
     },
-    {
+    {   key : 2,
         title : "Mousiko Kanali",
         url : "http://185.78.220.57:9210/stream.mp3",
         icon : "https://cdn.e-radio.gr/logos/gr/big/musikokanali.png"
     },
-    {
+    {   key : 3,
         title : "Talos FM 88.6",
         url : "http://eco.onestreaming.com:8840/stream.mp3",
         icon : "https://cdn.e-radio.gr/logos/gr/big/talosfm886.png"
+    },
+    {   key : 4,
+        title : "Studio 98 FM",
+        url : "http://sc2.streamwithq.com:8120/stream.mp3",
+        icon : "https://cdn.e-radio.gr/logos/gr/big/studio980.png"
+    },
+    {   key : 5,
+        title : "Astro FM 96.4",
+        url : "http://eco.onestreaming.com:8099/;",
+        icon : "https://cdn.e-radio.gr/logos/gr/big/radioastro.png"
+    },
+    {   key : 6,
+        title : "Capital Radio 90.7",
+        url : "http://185.78.220.57:9208/;",
+        icon : "https://cdn.e-radio.gr/logos/gr/big/capitalradio.png"
+    },
+    {   key : 7,
+        title : "Ράδιο Μεγαλόνησος 89.8",
+        url : "http://s3.onweb.gr:8540/;",
+        icon : "https://cdn.e-radio.gr/logos/gr/big/megalonisos898.png"
+    },
+    {   key : 8,
+        title : "Alpha FM 104.7",
+        url : "http://78.47.56.126:9219/stream",
+        icon : "https://cdn.e-radio.gr/logos/gr/big/alpharethymno.png"
     }
 ];
 
