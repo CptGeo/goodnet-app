0872c86874d2fca94c318cda8d5122228453b0d7
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-16 01:39:01 +0300

    Created a grid for all the radios. Added a header.

--- screens/Radio.js
+++ screens/Radio.js
@@ -4,73 +4,47 @@ import { Text, View, StyleSheet, Button, FlatList, ScrollView} from "react-nativ
 import playlists from "../settings/streams";
 import RadioStationPicker from "../components/RadioStationPicker";
 import AudioPlayer from "../components/AudioPlayer";
-
+import {FlatGrid, SectionGrid} from "react-native-super-grid";
 
 export default function Radio() {
-
-
-  const radioItems = playlists.map( item => {
-    return (
-      <RadioStationPicker
-        key={item.key}
-        stationImage={item.icon}
-        stationTitle={item.title}
-        stationUrl={item.url}
-      />
-    )
-  });
-
   return (
     <View style={styles.container}>
-        <FlatList
-          data={playlists}
-          renderItem={({ item }) => (
-            <View styles={styles.gridItem}>
-              <RadioStationPicker
-                key={item.key}
-                stationImage={item.icon}
-                stationTitle={item.title}
-                stationUrl={item.url}
-              />
-            </View>
-            )}
-          //Setting the number of column
-          numColumns={2}
-          keyExtractor={(item, index) => index}
-        />
-      {/* <AudioPlayer 
-            streamTitle={"Team FM"}
-            streamUri={"http://185.78.220.57:9200/stream.mp3"} 
-            streamImg={"https://cdn.e-radio.gr/logos/gr/big/teamfm.png"}
-        />
-        <AudioPlayer 
-            streamTitle={"Mousiko Kanali"}
-            streamUri={"http://185.78.220.57:9210/stream.mp3"} 
-            streamImg={"https://cdn.e-radio.gr/logos/gr/big/musikokanali.png"}
-        /> */}
+      <View>
+        <Text>Άκου Ραδιόφωνο</Text>
+      </View>
+      <FlatGrid
+        itemDimension={100}
+        data={playlists}
+        style={styles.gridView}
+        // staticDimension={300}
+        // fixed
+        spacing={10}
+        renderItem={({ item }) => (
+          <RadioStationPicker
+            key={item.key}
+            stationImage={item.icon}
+            stationTitle={item.title}
+            stationUrl={item.url}
+          />
+        )}
+      />
+      <AudioPlayer 
+        streamUri={"http://185.78.220.57:9210/stream.mp3"}
+        streamTitle={"Mousiko Kanali"}
+        streamImg={"https://cdn.e-radio.gr/logos/gr/big/musikokanali.png"}
+      />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  gridView: {
+    marginTop: 10,
     flex: 1,
-    backgroundColor: "#222",
   },
-  button: {
-    width: 50,
-    height: 50,
-  },
-  gridItem : {
-    width : 150,
-    height : 150,
-    backgroundColor: "#ff0000"
+  container : {
+    flex : 1,
+    backgroundColor : "#eee"
   }
-  // stationsGrid: {
-  //   flex: 1,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between',
-  //   alignItems : "center",
-  //   alignContent: 'center',
-  // }
 });
