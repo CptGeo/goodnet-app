93c43c2d94fb89d85fefaacb193a5e6d63f42107
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-16 01:39:12 +0300

    Added a header

--- screens/Home.js
+++ screens/Home.js
@@ -3,18 +3,26 @@
  */
 import GoodnetViewF from "../components/GoodnetViewF";
 import React from "react";
-import {StyleSheet} from "react-native";
+import {StyleSheet, View, Text} from "react-native";
 
 export default Home = (props) => {
 
     return (
-      <GoodnetViewF style={styles.container}/>
+      <View style={styles.container}>
+        <View>
+          <Text>Πρωτοσέλιδο - GoodNet.gr</Text>
+        </View>
+        <GoodnetViewF style={styles.webViewContainer}/>
+      </View>
     )
 }
 
 
 const styles = StyleSheet.create({
     container: {
+      flex: 1
+    },
+    webViewContainer: {
       flex: 1,
       justifyContent : "center",
       alignItems : "center"
