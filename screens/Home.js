1f63f72e905e505827ba4949c57f6d7aa492a6de
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-18 02:00:42 +0300

    Created headers for both screens. Homescreen has a menu as well

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,30 @@
 /**
  * Home is the default screen
  */
-import GoodnetViewF from "../components/GoodnetViewF";
+import GoodnetView from "../components/GoodnetView";
 import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import {TouchableOpacity} from 'react-native-gesture-handler'
+import {StyleSheet, View, Text, Image} from "react-native";
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 
-export default Home = (props) => {
+export default Home = ({navigation}) => {
 
     return (
       <View style={styles.container}>
-        <View>
-          <Text>Πρωτοσέλιδο - GoodNet.gr</Text>
+        <View style={styles.header}>
+          <View style={styles.headerImageWrapper}>
+            <Image style={styles.headerImage} source={require('../assets/goodnet_logo.png')} />
+          </View>
+          <View style={styles.headerTextWrapper}>
+            <Text style={styles.headerText}>GOODnet.gr</Text>
+          </View>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <Icon size={34} color="#fff" name={"menu"} />
+          </TouchableOpacity>
         </View>
-        <GoodnetViewF style={styles.webViewContainer}/>
+        <View style={styles.container}>
+          <GoodnetView style={styles.webViewContainer}/>
+        </View>
       </View>
     )
 }
@@ -20,11 +32,36 @@ export default Home = (props) => {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1
+      flex: 1,
+      position: "relative"
     },
     webViewContainer: {
       flex: 1,
       justifyContent : "center",
       alignItems : "center"
+    },
+    header : {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      backgroundColor: "#222",
+      paddingRight : 10,
+      maxHeight : 70,
+    },
+    headerText: {
+      color: "#fff",
+      fontSize : 20
+    },
+    headerImageWrapper: {
+      padding : 10,
+      backgroundColor: "#fff"
+    },
+    headerImage: {
+      width: 70,
+      height: "100%"
+    },
+    headerTextWrapper : {
+      flex: 4,
+      marginLeft : 20
     }
   });
--- screens/Radio.js
+++ screens/Radio.js
@@ -1,16 +1,24 @@
 import React, {useState} from "react";
-import { Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, Image} from "react-native";
 import AudioPlayer from "../components/AudioPlayer";
 
 
 export default function Radio() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Άκου Ραδιόφωνο</Text>
+      <View style={styles.header}>
+        <View style={styles.headerImageWrapper}>
+          <Image style={styles.headerImage} source={require('../assets/goodnet_logo.png')} />
+        </View>
+        <View style={styles.headerTextWrapper}>
+          <Text style={styles.headerText}>Άκου Ραδιόφωνο</Text>
+        </View>
+        {/* <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <Icon size={34} color="#fff" name={"menu"} />
+        </TouchableOpacity> */}
       </View>
-        <AudioPlayer 
-        />
+      <AudioPlayer 
+      />
     </View>
 
   );
@@ -21,5 +29,29 @@ const styles = StyleSheet.create({
   container : {
     flex : 1,
     backgroundColor : "#eee"
-  }
+  },
+  header : {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#222",
+    paddingRight : 10,
+    maxHeight : 70,
+  },
+  headerText: {
+    color: "#fff",
+    fontSize : 20
+  },
+  headerImageWrapper: {
+    padding : 10,
+    backgroundColor: "#fff"
+  },
+  headerTextWrapper : {
+    flex: 4,
+    marginLeft : 20
+  },
+  headerImage: {
+    width: 70,
+    height: "100%"
+  },
 });
