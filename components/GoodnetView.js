986a0abee09c9a9d75b8df674ef0e0737bdffce4
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-18 02:00:04 +0300

    Started to implement an Add to favourites functionality

--- components/GoodnetView.js
+++ components/GoodnetView.js
@@ -1,92 +1,107 @@
-import React, {Component, useState} from "react";
+import React, {Component, useContext, useState} from "react";
 import {WebView} from "react-native-webview";
 import {Text, StyleSheet, View, ActivityIndicator} from "react-native";
+import UrlContext from "./UrlContext";
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
+import {TouchableOpacity} from 'react-native-gesture-handler'
 
-class GoodnetView extends Component{
+// import * as SQLite from 'expo-sqlite';
+
+// const db = SQLite.openDatabase("favourites");
+
+export default function GoodnetView(props){
+    const urlContext = useContext(UrlContext);
+    const jsCode = `let everything = document.querySelectorAll("*"); everything.forEach(item => {item.style.userSelect = 'none'})`;
 
 
-    constructor(props){
-        super();
-
-        //string
-        this.homepageUrl = props.settings.url;
-
-        //string
-        this.urlParam = props.settings.urlMobParam.name + "=" + props.settings.urlMobParam.value;
-
-        //string
-        this.uri = props.settings.url;
-
-        //WebView Object
-        this.webview = null;
-
-        this.count = 0;
-
-        this.state = {
-            changingPage : false,
-            previousPageUrl : '',
-            nextPageUrl : ''
-        }
-    }
-
-    urlHasParams = (url) => {
-        const urlregex = /(\?|\&)([^=]+)\=([^&]+)/;
-        return url.match(urlregex) ? true : false;
+    
+    const isFavourite = () => {
+        //check if url exists in favourites
+        return false;
     }
 
 
-    render(){
+    const [favourite, setFavourite] = useState( () => {
+        isFavourite(urlContext.value);
+    });
 
-        return(
+
+    const toggleFavorite = () => {
+        //Check if already added 
+        const query = "UPDATE"
+        console.log("Added to favourites: " + urlContext.value);
+    }
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.favoritesBtn}>
+                <TouchableOpacity onPress={() => toggleFavorite()}>
+                    {favourite ? 
+                        (<Icon size={34} color="#db2625" name={"heart"} />) : 
+                        (<Icon size={34} color="#db2625" name={"heart-outline"} />)
+                    }
+                </TouchableOpacity>
+            </View>
             <WebView 
                 source={
                     {
-                        uri : this.uri,
-                        headers : {
-                            Cookie: 'goodnet_mobile=1'
-                        }
+                        uri : urlContext.value
                     }
                 }
                 sharedCookiesEnabled={true}
-                styles={styles.container}
+                styles={styles.webview}
                 startInLoadingState={true}
                 pullToRefreshEnabled={true}
                 scalesPageToFit={true}
+                javaScriptEnabled={true}
+                injectedJavaScript={jsCode}
+                userAgent={'GoodnetMobile/1.0.3'} // user agent for identification
+                //applicationNameForUserAgent={'GoodnetMobile/1.0.3'} // user agent for identification
                 renderLoading={() => (
                     <ActivityIndicator 
                         color='#4374ca' 
                         size='large' 
                         style={styles.container}
                     />)}
-                ref={(ref) => this.webview = ref}
+                onNavigationStateChange={(navState) => {
+                    if(navState.loading){
+                        return (
+                            <ActivityIndicator 
+                                    color='#4374ca' 
+                                    size='large' 
+                                    style={styles.container}
+                            />
+                        )
+                    }
+                    urlContext.handler(navState.url);
+                }}
                 // onNavigationStateChange={this.webviewNavigationChange}
                 // onShouldStartLoadWithRequest={this.shouldStartLoadWithRequest}
             />
-        );
-    }
+        </View>
+    );
+
 }
-
-
-
 
 
 const styles = StyleSheet.create({
     container : {
-        flex: 1,
-        flexGrow: 1,
-        alignItems : 'center',
-        justifyContent : "center",
-        position : "absolute",
-        width : "100%",
-        height : "100%",
-        left: 0,
-        top : 0
+        flex: 1
     },
+    webview : {
+        flex: 1,
+        position : "relative",
+        zIndex: 1
+    },
+    favoritesBtn: {
+        position: "absolute",
+        top: 10,
+        right: 10,
+        zIndex: 100,
+        width : 34,
+        height : 34,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+    }
 });
-
-
-export default GoodnetView;
-
-
-
-
