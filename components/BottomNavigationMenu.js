aa31fb1c90f4982770467b368c08dff492d7f3e7
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-21 01:08:51 +0300

    Cleaned and converted to functional component

--- components/BottomNavigationMenu.js
+++ components/BottomNavigationMenu.js
@@ -1,121 +1,76 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import {View, Text} from "react-native";
-import BottomNavigation, {FullTab, ShiftingTab} from "react-native-material-bottom-navigation";
-
+import BottomNavigation, {ShiftingTab} from "react-native-material-bottom-navigation";
 import UrlContext from "./UrlContext";
-
-//more icons https://icons.expo.fyi/
-import Icon from '@expo/vector-icons/MaterialCommunityIcons'
-
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import CategoriesNavigation from "./CategoriesNavigation";
 import Radio from "../screens/Radio";
+import tabs from "../settings/tabs";
 
-export default class BottomNavigationMenu extends React.Component{
 
 
-    static contextType = UrlContext;
-    tabs = [
-        {
-            key: 'frontpage',
-            icon: 'book-open-page-variant',
-            label: 'Πρωτοσέλιδο',
-            barColor: '#222222',
-            pressColor: '#4273c9'
-        },
-        {
-            key: 'user_message',
-            icon: 'message',
-            label: 'Στείλε μήνυμα',
-            barColor: '#222222',
-            pressColor: '#4273c9'
-        },
-        {
-            key: 'listen_radio',
-            icon: 'radio',
-            label: 'Άκου Ραδιόφωνο',
-            barColor: '#222222',
-            pressColor: '#4273c9'
-        },
-        {
-            key: 'read_later',
-            icon: 'pin',
-            label: 'Read later',
-            barColor: '#222222',
-            pressColor: '#4273c9'
-        },
-        {
-            key: 'more',
-            icon: 'dots-horizontal',
-            label: 'Περισσότερα',
-            barColor: '#222222',
-            pressColor: '#4273c9'
-        },
-      ]
+export default function BottomNavigationMenu(){
 
-    state = {
-        activeTab: 'frontpage'
-    }
 
-    renderIcon = icon => ({ isActive }) => (
+    const urlCtx = useContext(UrlContext);
+
+    const [activeTab, setActiveTab] = useState('frontpage');
+
+    const renderIcon = icon => ({ isActive }) => (
         <Icon size={26} color="white" name={icon} />
     );
 
-
-    renderTab = ({ tab, isActive }) => (
+    const renderTab = ({ tab, isActive }) => (
         <ShiftingTab
             isActive={isActive}
             key={tab.key}
             label={tab.label}
-            renderIcon={this.renderIcon(tab.icon)}
-    />
-    )
+            renderIcon={renderIcon(tab.icon)}
+        />
+    );
 
-    tabPress = (activeTab) => {
+    const tabPress = (activeTab) => {
         switch (activeTab.key) {
             case "frontpage" : {
-                this.setState({activeTab : "frontpage"})
-                this.context.handler("https://goodnet.gr");
+                setActiveTab("frontpage");
+                urlCtx.handler("https://goodnet.gr");
                 break;
             }
             case "user_message" : {
-                this.setState({activeTab : "user_message"})
+                setActiveTab("user_message");
                 break;
             }
             case "listen_radio" : {
-                this.setState({activeTab : "listen_radio"})
+                setActiveTab("listen_radio");
                 break;
             }
             case "read_later" : {
-                this.setState({activeTab : "read_later"})
+                setActiveTab("read_later");
                 break;
             }
             case "more" : {
-                this.setState({activeTab : "more"})
+                setActiveTab("more");
                 break;
             } 
             default : {
-                this.setState({activeTab : "frontpage"})
+                setActiveTab("frontpage");
             }
         }
     }
 
-
-    render() {
-        return (
-          <View style={{ flex: 1 }}>
+    return (
+        <View style={{ flex: 1 }}>
             <View style={{ flex: 1 }}>
-                {this.state.activeTab === "frontpage" ? <CategoriesNavigation /> : 
-                this.state.activeTab === "listen_radio" ? <Radio /> : 
+                {activeTab === "frontpage" ? <CategoriesNavigation /> : 
+                activeTab === "listen_radio" ? <Radio /> : 
                 <Text>No page</Text>}
             </View>
             <BottomNavigation
-                activeTab={this.state.activeTab}
-                onTabPress={activeTab => {this.tabPress(activeTab)}}
-                renderTab={this.renderTab}
-                tabs={this.tabs}
+                activeTab={activeTab}
+                onTabPress={tab => {tabPress(tab)}}
+                renderTab={renderTab}
+                tabs={tabs}
             />
-          </View>
-        )
-      }
-    
+        </View>
+    )
 }
