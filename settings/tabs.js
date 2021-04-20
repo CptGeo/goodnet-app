e3d7c5ae824a98bfe51732ffd242aed2414f4780
CptGeo <georgekalyvianakis18@gmail.com>
2021-04-21 01:10:58 +0300

    Seperated the tabs of BottomNavigation for modularity

---
+++ settings/tabs.js
@@ -0,0 +1,42 @@
+export default tabs = [
+    {
+        id : 10,
+        key: 'frontpage',
+        icon: 'book-open-page-variant',
+        label: 'Πρωτοσέλιδο',
+        barColor: '#222222',
+        pressColor: '#4273c9'
+    },
+    {
+        id : 20,
+        key: 'user_message',
+        icon: 'message',
+        label: 'Στείλε μήνυμα',
+        barColor: '#222222',
+        pressColor: '#4273c9'
+    },
+    {
+        id : 30,
+        key: 'listen_radio',
+        icon: 'radio',
+        label: 'Άκου Ραδιόφωνο',
+        barColor: '#222222',
+        pressColor: '#4273c9'
+    },
+    {
+        id : 40,
+        key: 'read_later',
+        icon: 'pin',
+        label: 'Read later',
+        barColor: '#222222',
+        pressColor: '#4273c9'
+    },
+    {
+        id : 50,
+        key: 'more',
+        icon: 'dots-horizontal',
+        label: 'Περισσότερα',
+        barColor: '#222222',
+        pressColor: '#4273c9'
+    },
+];
